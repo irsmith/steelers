f65b0f6bb5239a803f3c4977283fe653ea7ad003
nidhisaini <nidhi_sainii@yahoo.com>
2013-09-21 16:48:34 -0700

    splash xml,java and kissandstop.png

---
+++ MainActivity/src/net/skup/knstop/SplashScreen.java
@@ -0,0 +1,41 @@
+package net.skup.knstop;
+
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreen extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash_screen);
+		Thread logoTimer = new Thread() {
+			public void run(){
+				try{
+					int logoTimer = 0;
+					//logo timer runs for 2 seconds
+					while(logoTimer < 2000){
+						sleep(100);
+						logoTimer = logoTimer +100;
+					};
+					// startActivity(new Intent(SplashActivity.this, MainActivity.class));
+				} 
+
+				catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+				finally{
+					finish();
+				}
+			}
+		};
+
+		logoTimer.start();
+
+	}
+}
